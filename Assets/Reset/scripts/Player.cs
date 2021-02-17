520a62a527c897cba52c08c1db02e47fe6d00283
Joshua Cadavez <cadav001@umn.edu>
2021-02-16 20:39:06 -0500

    Boxer speed

---
+++ Assets/Reset/scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    Vector2 moveInput;
+
+    private Rigidbody2D body2D;
+    private SpriteRenderer renderer2D;
+
+    void Awake() {
+        body2D = GetComponent<Rigidbody2D>();
+        renderer2D = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate() {
+        Vector2 pos = body2D.position + moveInput * moveSpeed;
+        body2D.MovePosition(pos); 
+    }
+}
