d9b8f018f9c1b4c2f045ed6ad766b5ac78a71034
Joshua Cadavez <cadav001@umn.edu>
2021-02-19 23:41:19 -0500

    Add PlayerController

--- Assets/Reset/scripts/Player.cs
+++ Assets/Reset/scripts/Player.cs
@@ -9,18 +9,19 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D body2D;
     private SpriteRenderer renderer2D;
+    private PlayerController controller;
 
     void Awake() {
         body2D = GetComponent<Rigidbody2D>();
         renderer2D = GetComponent<SpriteRenderer>();
+        controller = GetComponent<PlayerController>();
     }
-
+    
     void Update()
     {
-        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-    }
-
-    void FixedUpdate() {
+        moveInput = new Vector2(controller.moving.x, controller.moving.y);
+        renderer2D.flipX = controller.flip_x;
+        renderer2D.flipY = controller.flip_y;
         Vector2 pos = body2D.position + moveInput * moveSpeed;
         body2D.MovePosition(pos); 
     }
---
+++ Assets/Reset/scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 moving = new Vector2();
+    public bool flip_x, flip_y;
+
+    void Update()
+    {
+        moving.x = moving.y = 0;
+        float move_x = Input.GetAxis("Horizontal");
+        float move_y = Input.GetAxis("Vertical");
+        if (move_x != 0)
+        {
+            flip_x = move_x > 0;
+            moving.x = flip_x ? 1 : -1;
+        }
+        if (move_y != 0)
+        {
+            flip_y = move_y > 0;
+            moving.y = flip_y ? 1 : -1;
+        }
+    }
+}
